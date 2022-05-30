b65da2f4c3bb0b59b81b47c988f2bd2d214fa251
John Xander <johnblox5@gmail.com>
2022-05-30 22:53:49 +0100

    There are some problems with this

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 const wordList = [
-    "aaaas", "aaasa", "aasaa", "asaaa", "saaaa"
+    "strawberry", "friendship", "everything", "appreciate", "motivation",
+    "abilities", "adulthood", "expertise", "technique", "recognise",
+    "blizzard", "dazzling", "aardvark", "exorcism", "academic",
+    "jacuzzi", "abandon", "journey", "license", "example"
 ]
 
 const word = document.getElementById("word")
 const btn = document.getElementById("btn")
 const lives = document.getElementById("lives")
+const bangPic = document.getElementById("bangPic")
 
 let currentWord = "STRAWBERRY"
 let hiddenWord 
@@ -16,6 +20,8 @@ const addWord = () => {
     currentWord = wordList[randomIdx].toUpperCase()
     hiddenWord = "_".repeat(currentWord.length)
     word.innerHTML = hiddenWord
+    bangPic.src = "img/bang-pic-9.jpg"
+    lives.innerHTML = "9 lives remaining"
 }
 
 const findLetter = (letter) => {
@@ -30,6 +36,7 @@ const findLetter = (letter) => {
 
     const hiddenResult = hidWordArr.map((char, index) => {
         if (index === idx) char = letter
+        if (char !== "_") char = char
         return char
     }).join("")
 
@@ -37,10 +44,18 @@ const findLetter = (letter) => {
         word.innerHTML = hiddenResult
     } else {
         livesRemaining -= 1
-        lives.innerHTML = `${livesRemaining} lives remaining`
+        bangPic.src = `img/bang-pic-${livesRemaining}.jpg`
+
+        if (livesRemaining <= 0) {
+            lives.innerHTML = "Game over!"
+            bangPic.src = "img/bang-pic-0.jpg"
+        } else if (livesRemaining === 1) {
+            lives.innerHTML = `${livesRemaining} life remaining` 
+        } else {
+            lives.innerHTML = `${livesRemaining} lives remaining`
+        }
     }
 
-    console.log(livesRemaining)
 }
 
 
